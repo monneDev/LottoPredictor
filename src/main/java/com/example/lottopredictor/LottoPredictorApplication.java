e565b9a18a0a184344b2f582deab485a24b336e0
Simon <sipe0005@stud.kea.dk>
2025-09-22 13:13:47 +0200

    Initial commit

---
+++ src/main/java/com/example/lottopredictor/LottoPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lottopredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoPredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoPredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lottopredictor/LottoPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lottopredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoPredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
